cb8731c3202f1358db4c5891eac19d1c1e7daef0
samgasper <zakirov.ismail@sarkor.uz>
2026-03-08 14:13:39 +0500

    feat: added main gameplay feature: - self harm (when snakes eats it self) - movement with Y axis
    minor changes:
    - fixed food and snake body position collision
    - some code style changes

--- main.go
+++ main.go
@@ -69,8 +69,7 @@ var DirectionStatePointKeyMap = map[string]DirectionStatePoint{
 }
 
 type SnakePoint struct {
-	PositionX      int
-	PositionY      int
+	Point
 	DirectionState DirectionState
 }
 
@@ -79,7 +78,8 @@ type Model struct {
 	Width                int
 	Height               int
 	Snake                []SnakePoint
-	FoodPoint            Point
+	SnakePointMap        map[Point]int
+	FoodPoint            *Point
 	NextUpdateSnakePoint int
 }
 
@@ -89,40 +89,195 @@ func tick() tea.Cmd {
 	})
 }
 
-func getFoodPint() Point {
+func (m *Model) setFoodPint() {
 	y := rand.Intn(Height - 1)
 	x := rand.Intn(Width - 1)
 
-	return Point{Y: y, X: x}
+	point := Point{Y: y, X: x}
+	_, inSnakeBody := m.SnakePointMap[point]
+	if inSnakeBody {
+		m.FoodPoint = nil
+		return
+	}
+
+	m.FoodPoint = &point
 }
 
-func (m *Model) isSnake(point Point, snakeMap map[Point]struct{}) bool {
-	_, exists := snakeMap[point]
+func (m *Model) updateSnakePointMap() {
+	m.SnakePointMap = make(map[Point]int, cap(m.Snake))
+
+	for i, p := range m.Snake {
+		m.SnakePointMap[p.Point] = i
+	}
+}
+
+func (m *Model) isSnake(point Point) bool {
+	_, exists := m.SnakePointMap[point]
 	return exists
 }
 
 func (m *Model) isFood(point Point) bool {
-	return point == m.FoodPoint
+	if m.FoodPoint == nil {
+		return false
+	}
+
+	return point == *m.FoodPoint
 }
 
 func (m *Model) isBorder(point Point) bool {
 	return point.X == m.Width-1 || point.X == 0
 }
 
+func (m *Model) onKeyMsgUpdate(msgStr string) (tea.Model, tea.Cmd) {
+	if msgStr == "q" || msgStr == "ctrl+c" {
+		return m, tea.Quit
+	}
+
+	dStatePoint, exists := DirectionStatePointKeyMap[msgStr]
+
+	if exists {
+		head := m.Snake[len(m.Snake)-1]
+		if head.DirectionState == dStatePoint.DirectionState {
+			return m, nil
+		}
+
+		opositeDirection := OpositeDirectionMap[head.DirectionState]
+		if opositeDirection == dStatePoint.DirectionState {
+			return m, nil
+		}
+		m.Ticking = false
+
+		x := head.X + dStatePoint.X
+		y := head.Y + dStatePoint.Y
+
+		head.DirectionState = dStatePoint.DirectionState
+		head.X = x
+		head.Y = y
+
+		if m.hasBorderCollision(head) {
+			fmt.Println("YOU SUCK")
+			return m, tea.Quit
+		}
+
+		head = m.handleYOutOfBorder(head)
+
+		m.handleSelfHarm(head)
+		m.handleEat(head)
+		m.handleMovement(head)
+
+		return m, nil
+	}
+
+	return m, nil
+}
+
+func (m *Model) onTickMsg() (tea.Model, tea.Cmd) {
+	if !m.Ticking {
+		m.Ticking = true
+		return m, tick()
+	}
+
+	head := m.Snake[len(m.Snake)-1]
+	switch head.DirectionState {
+	case DirectionStateX:
+		head.X += 1
+
+	case DirectionStateXReverse:
+		head.X -= 1
+
+	case DirectionStateYReverse:
+		head.Y -= 1
+
+	case DirectionStateY:
+		head.Y += 1
+	}
+
+	if m.hasBorderCollision(head) {
+		fmt.Println("YOU SUCK")
+		return m, tea.Quit
+	}
+	head = m.handleYOutOfBorder(head)
+
+	m.handleSelfHarm(head)
+	m.handleEat(head)
+	m.handleMovement(head)
+
+	return m, tick()
+}
+
+func (m *Model) hasBorderCollision(head SnakePoint) bool {
+	return head.X < 0 || head.X >= m.Width-1
+}
+
+func (m *Model) handleYOutOfBorder(head SnakePoint) SnakePoint {
+	switch {
+	case head.Y > m.Height:
+		head.Y = 0
+
+	case head.Y < 0:
+		head.Y = m.Height
+	}
+
+	return head
+}
+
+func (m *Model) handleEat(head SnakePoint) {
+	isHeadOnFoodPoint := head.X == m.FoodPoint.X && head.Y == m.FoodPoint.Y
+	if !isHeadOnFoodPoint {
+		return
+	}
+
+	tale := m.Snake[0]
+	newSnakePoint := SnakePoint{
+		Point: Point{
+			X: tale.X - 1,
+			Y: tale.Y - 1,
+		},
+		DirectionState: tale.DirectionState,
+	}
+
+	m.Snake = append([]SnakePoint{newSnakePoint}, m.Snake...)
+	m.setFoodPint()
+}
+
+func (m *Model) handleSelfHarm(head SnakePoint) {
+	bodyIndex, headInOwnBody := m.SnakePointMap[head.Point]
+	if !headInOwnBody {
+		return
+	}
+
+	m.Snake = m.Snake[bodyIndex:len(m.Snake)]
+}
+
+func (m *Model) handleMovement(head SnakePoint) {
+	m.Snake = append(m.Snake, head)
+	m.Snake = m.Snake[1:len(m.Snake)]
+}
+
 func initialModel() *Model {
+	snakeStartPoint := Point{
+		X: 1,
+		Y: 0,
+	}
+
 	snake := make([]SnakePoint, 0, Width*Height)
 	snake = append(snake, SnakePoint{
-		PositionX:      1,
-		PositionY:      0,
+		Point:          snakeStartPoint,
 		DirectionState: DirectionStateX,
 	})
-
-	return &Model{
-		Width:     Width,
-		Height:    Height,
-		Snake:     snake,
-		FoodPoint: getFoodPint(),
+	snakeMap := map[Point]int{
+		snakeStartPoint: 0,
 	}
+
+	m := &Model{
+		Width:         Width,
+		Height:        Height,
+		Snake:         snake,
+		SnakePointMap: snakeMap,
+	}
+	m.setFoodPint()
+
+	return m
 }
 
 func (m *Model) Init() tea.Cmd {
@@ -132,10 +287,11 @@ func (m *Model) Init() tea.Cmd {
 func (m *Model) View() string {
 	var b strings.Builder
 
-	snakeMap := make(map[Point]struct{}, len(m.Snake))
-	for _, p := range m.Snake {
-		snakeMap[Point{X: p.PositionX, Y: p.PositionY}] = struct{}{}
-	}
+	m.updateSnakePointMap()
+
+	b.WriteString(fmt.Sprintf("SCORE: %d", len(m.Snake)-1))
+	b.WriteRune('\n')
+	b.WriteRune('\n')
 
 	for y := 0; y < m.Height; y++ {
 		for x := 0; x < m.Width; x++ {
@@ -143,7 +299,7 @@ func (m *Model) View() string {
 			point := Point{X: x, Y: y}
 
 			switch {
-			case m.isSnake(point, snakeMap):
+			case m.isSnake(point):
 				b.WriteString("██")
 
 			case m.isFood(point):
@@ -164,94 +320,16 @@ func (m *Model) View() string {
 }
 
 func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	if m.FoodPoint == nil {
+		m.setFoodPint()
+	}
+
 	switch msg := msg.(type) {
 	case TickMsg:
-		if !m.Ticking {
-			m.Ticking = true
-			return m, tick()
-		}
-
-		head := m.Snake[len(m.Snake)-1]
-		switch head.DirectionState {
-		case DirectionStateX:
-			head.PositionX += 1
-
-		case DirectionStateXReverse:
-			head.PositionX -= 1
-
-		case DirectionStateYReverse:
-			head.PositionY -= 1
-
-		case DirectionStateY:
-			head.PositionY += 1
-		}
-
-		if head.PositionX < 0 || head.PositionX >= m.Width-1 || head.PositionY < 0 || head.PositionY >= m.Height {
-			fmt.Println("YOU SUCK")
-			return m, tea.Quit
-		}
-
-		if head.PositionX == m.FoodPoint.X && head.PositionY == m.FoodPoint.Y {
-			tale := m.Snake[0]
-			newSnakePoint := SnakePoint{
-				PositionX:      tale.PositionX - 1,
-				PositionY:      tale.PositionY - 1,
-				DirectionState: tale.DirectionState,
-			}
-
-			m.Snake = append([]SnakePoint{newSnakePoint}, m.Snake...)
-			m.FoodPoint = getFoodPint()
-		}
-
-		m.Snake = append(m.Snake, head)
-		m.Snake = m.Snake[1:len(m.Snake)]
-
-		return m, tick()
+		return m.onTickMsg()
 
 	case tea.KeyMsg:
-		msgStr := msg.String()
-		if msgStr == "q" || msgStr == "ctrl+c" {
-			return m, tea.Quit
-		}
-
-		dStatePoint, exists := DirectionStatePointKeyMap[msgStr]
-
-		if exists {
-			head := m.Snake[len(m.Snake)-1]
-			if head.DirectionState == dStatePoint.DirectionState {
-				return m, nil
-			}
-
-			opositeDirection := OpositeDirectionMap[head.DirectionState]
-			if opositeDirection == dStatePoint.DirectionState {
-				return m, nil
-			}
-			m.Ticking = false
-
-			x := head.PositionX + dStatePoint.X
-			y := head.PositionY + dStatePoint.Y
-
-			head.DirectionState = dStatePoint.DirectionState
-			head.PositionX = x
-			head.PositionY = y
-
-			if head.PositionX == m.FoodPoint.X && head.PositionY == m.FoodPoint.Y {
-				tale := m.Snake[0]
-				newSnakePoint := SnakePoint{
-					PositionX:      tale.PositionX - 1,
-					PositionY:      tale.PositionY - 1,
-					DirectionState: tale.DirectionState,
-				}
-
-				m.Snake = append([]SnakePoint{newSnakePoint}, m.Snake...)
-				m.FoodPoint = getFoodPint()
-			}
-
-			m.Snake = append(m.Snake, head)
-			m.Snake = m.Snake[1:len(m.Snake)]
-
-			return m, nil
-		}
+		return m.onKeyMsgUpdate(msg.String())
 	}
 
 	return m, nil
